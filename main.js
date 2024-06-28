7b06d35b9bd77a12b39f98606750c139cee385e6
anasxonbahodirov <anasxonbahodirov1@gmail.com>
2024-06-28 15:46:05 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+alert('Sizda xozir random sonlari chiqishni boshledi😊')
+alert('Random orqali chiqqan son: 22')
+let a = confirm('Biz random sonni ikkiga kopaytirmoqchimiz va bolmoqchimiz')
+alert('Random orqali chiggan son: 22 edi biz uni 2 ga kopaytlrib 44 sonini chigardik, va shu sonni 2 ga bolib: 11 sonini chigardik, va shu sonni 3 ga bolgandagi qoldig: 1 sonini chiqardik')
+
+
+
+alert('Sizda xozir random sonlari chiqishni boshledi😜')
+alert('Random orqali chiqqan son: 55')
+let b = confirm('Biz random sonni ikkiga kopaytirmoqchimiz va bolmoqchimiz')
+alert('Random orqali chiqqan son: 55 edi biz uni 2 ga kopaytirib 110 sonini chiqardik, va shu sonni 2 ga bolib: 27.5 sonini chigardik, va shu sonni 3 ga bolgandagi qoldiq: 1 sonini chiqardik')
+alert('Korganingiz uchun tashakur')
